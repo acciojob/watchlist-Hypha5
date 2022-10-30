f197cb218ba7ca6efff5824acfa785e1b5182950
HP <amrinhypha@gmail.com>
2022-10-30 20:04:30 +0530

    changes in delete function

--- src/main/java/com/driver/MovieRepository.java
+++ src/main/java/com/driver/MovieRepository.java
@@ -77,6 +77,7 @@ public class MovieRepository {
         if(movieDirectorHashMap.containsKey(name))
         {
              temp = movieDirectorHashMap.get(name);
+             deleteformmovies(temp);
             movieDirectorHashMap.remove(name);
         }
         for(Director director:directorList)
@@ -84,7 +85,7 @@ public class MovieRepository {
             if(director.name.equals(name))
                 directorList.remove(director);
         }
-        deleteformmovies(temp);
+
     }
     void deleteAllDirectors()
     {
